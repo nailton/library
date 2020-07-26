ce07f96ec2a47aec576a8e6d744f5e4973269413
Nailton Sousa <nailton.osn@gmail.com>
2020-07-26 12:21:55 -0300

    PASS  Tests\Unit\BookReservationsTest  -  a user can check out a book twice

--- tests/Unit/BookReservationsTest.php
+++ tests/Unit/BookReservationsTest.php
@@ -40,4 +40,28 @@ class BookReservationsTest extends TestCase {
         $this->assertEquals(now(), Reservation::first()->checked_in_at);
 
     }
+
+    /** @test */
+    public function a_user_can_check_out_a_book_twice() {
+        $book = factory(Book::class)->create();
+        $user = factory(User::class)->create();
+        $book->checkout($user);
+        $book->checkin($user);
+
+        $book->checkout($user);
+
+        $this->assertCount(2, Reservation::all());
+        $this->assertEquals($user->id, Reservation::find(2)->user_id);
+        $this->assertEquals($book->id, Reservation::find(2)->book_id);
+        $this->assertNull(Reservation::find(2)->checked_in_at);
+        $this->assertEquals(now(), Reservation::find(2)->checked_out_at);
+
+        $book->checkin($user);
+
+        $this->assertCount(2, Reservation::all());
+        $this->assertEquals($user->id, Reservation::find(2)->user_id);
+        $this->assertEquals($book->id, Reservation::find(2)->book_id);
+        $this->assertNotNull(Reservation::find(2)->checked_in_at);
+        $this->assertEquals(now(), Reservation::find(2)->checked_in_at);
+    }
 }
