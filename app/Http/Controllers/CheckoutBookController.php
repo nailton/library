a82e1d24b8ffa78f0d19aef20f5bdc9c152cc631
Nailton Sousa <nailton.osn@gmail.com>
2020-08-03 22:51:24 -0300

    PASS  Tests\Feature\BookCheckoutTest - a book can be checked out by a signed in user

--- app/Http/Controllers/CheckoutBookController.php
+++ app/Http/Controllers/CheckoutBookController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use App\User;
+use Auth;
 
 class CheckoutBookController extends Controller {
     public function store(Book $book) {
--- tests/Feature/BookCheckoutTest.php
+++ tests/Feature/BookCheckoutTest.php
@@ -6,26 +6,21 @@ use App\Book;
 use App\Reservation;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class BookCheckoutTest extends TestCase {
-    use WithoutMiddleware;
     use RefreshDatabase;
 
     /** @test */
     public function a_book_can_be_checked_out_by_a_signed_in_user() {
-        $this->withoutExceptionHandling();
-
         $book = factory(Book::class)->create();
 
-        $this->actingAs($user = factory(User::class)->create())
+        $response = $this->actingAs($user = factory(User::class)->create())
             ->post('/checkout/' . $book->id);
 
         $this->assertCount(1, Reservation::all());
         $this->assertEquals($user->id, Reservation::first()->user_id);
         $this->assertEquals($book->id, Reservation::first()->book_id);
         $this->assertEquals(now(), Reservation::first()->checked_out_at);
-
     }
 }
